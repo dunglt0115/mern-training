3b3105e9a3466978b0ff2315ea7698b59bec8020
tiendungid1 <tiendungid1@gmail.com>
2021-09-14 16:19:20 +0700

    Fix component & add file to fix netlify error

--- client/src/components/layout/NavBarMenu.js
+++ client/src/components/layout/NavBarMenu.js
@@ -1,4 +1,4 @@
-import NavBar from 'react-bootstrap/NavBar';
+import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import learnItLogo from '../../assets/logo.svg';
 import logoutIcon from '../../assets/logout.svg';
@@ -6,7 +6,7 @@ import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
-//3:40
+
 const NavBarMenu = () => {
     const {
         authState: {
@@ -18,15 +18,15 @@ const NavBarMenu = () => {
     const logout = () => logoutUser();
 
     return (
-        <NavBar expand='lg' bg='primary' variant='dark' className='shadow'>
-            <NavBar.Brand className='font-weight-bolder text-white'>
+        <Navbar expand='lg' bg='primary' variant='dark' className='shadow'>
+            <Navbar.Brand className='font-weight-bolder text-white'>
                 <img src={learnItLogo} alt='learnItLogo' width='32' height='32' className='mr-2' />
                 Welcome {username}
-            </NavBar.Brand>
+            </Navbar.Brand>
 
-            <NavBar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
 
-            <NavBar.Collapse id='basic-navbar-nav'>
+            <Navbar.Collapse id='basic-navbar-nav'>
                 <Nav className='mr-auto'>
                     <Nav.Link className='font-weight-bolder text-white' to='/dashboard' as={Link}>
                         Dashboard
@@ -52,8 +52,8 @@ const NavBarMenu = () => {
                         Logout
                     </Button>
                 </Nav>
-            </NavBar.Collapse>
-        </NavBar>
+            </Navbar.Collapse>
+        </Navbar>
     );
 };
 
